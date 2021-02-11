051cc5e333acd60d524d37d54bb8581204ba291b
sshwy <jy.cat@qq.com>
2021-02-11 12:24:13 +0800

    test: add test for label

--- test/t.js
+++ test/t.js
@@ -1,3 +1,3 @@
 const kramed = require('../index');
 
-console.log(kramed('I am using [# markdown].'));
+console.log(kramed('*斜体* **粗体** ~~删除~~ `inline code` [# 标签] [链接](https://sshwy.name) $a^2+b^2=c^2$.'));
