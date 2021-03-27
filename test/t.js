84b5570444b0e0f85ff4cb20abdd3d39977ff9c0
sshwy <jy.cat@qq.com>
2021-03-27 17:29:14 +0800

    fix(rules/block): fix bug of footnote render

--- test/t.js
+++ test/t.js
@@ -1,3 +1,3 @@
 const kramed = require('../index');
 
-console.log(kramed('*斜体* **粗体** ~~删除~~ `inline code` [# 标签] [链接](https://sshwy.name) $a^2+b^2=c^2$.'));
+console.log(kramed('*斜体* **粗体** ~~删除~~ `inline code` [# 标签] [链接](https://sshwy.name) $a^2+b^2=c^2$. 脚注[^1][^2][^3]\n\n第二行\n\n[^1]:https://notes.sshwy.name/\n[^2]:https://notes.sshwy.name/\n[^3]:https://notes.sshwy.name/\n'));
